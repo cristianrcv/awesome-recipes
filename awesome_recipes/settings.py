2905325da448b100b123f85da0cedf6158d18a70
CristianRCV <cristianrcv@gmail.com>
2018-09-26 12:14:02 +0200

    Update static root for deployment

--- awesome_recipes/settings.py
+++ awesome_recipes/settings.py
@@ -114,24 +114,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
-
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AUTHENTICATION
 LOGIN_URL = 'awesome_recipes_login'
-
 LOGOUT_URL = 'awesome_recipes_logout'
-
 LOGIN_REDIRECT_URL = 'recipes_list'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
